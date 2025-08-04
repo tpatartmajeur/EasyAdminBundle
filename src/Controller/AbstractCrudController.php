7fab1c86ea52f8592b6ef619d6539b5a8659d5a3
Timothy Prefol <tp@artmajeur.com>
2025-08-04 09:53:57 +0200

    Use `referrer` for SAVE_AND_RETURN action fallback

--- src/Controller/AbstractCrudController.php
+++ src/Controller/AbstractCrudController.php
@@ -639,7 +639,8 @@ abstract class AbstractCrudController extends AbstractController implements Crud
                 ->setAction(Action::EDIT)
                 ->setEntityId($context->getEntity()->getPrimaryKeyValue())
                 ->generateUrl(),
-            Action::SAVE_AND_RETURN => $this->container->get(AdminUrlGenerator::class)->setAction(Action::INDEX)->generateUrl(),
+            Action::SAVE_AND_RETURN => $context->getReferrer()
+                ?? $this->container->get(AdminUrlGenerator::class)->setAction(Action::DETAIL)->setEntityId($context->getEntity()->getPrimaryKeyValue())->generateUrl(),
             Action::SAVE_AND_ADD_ANOTHER => $this->container->get(AdminUrlGenerator::class)->setAction(Action::NEW)->generateUrl(),
             default => $this->generateUrl($context->getDashboardRouteName()),
         };
